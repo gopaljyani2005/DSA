48dd85973e08e7ce167001bcc431890192e69963
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-08-24 07:49:47 +0530

    Time: 4 ms (17.05%) | Memory: 65.6 MB (5.09%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -6,6 +6,8 @@ public:
         vector<int> perfix(size, 0);
         vector<int> suffix(size, 0);
         int sum;
+
+// calculate perfix sum of the given array and store;
         sum = 0;
         for(int i=0;i<size;i++){
             sum+=nums[i];
@@ -13,10 +15,10 @@ public:
                 perfix[i] = sum;
                 sum = 0;
             }
-            cout<<sum<<" ";
         }
-        cout<<endl;
+        ans = sum;
 
+// calculate suffix sum of array and store it ;
         sum = 0;
         for(int i=size-1;i>=0;i--){
             sum+=nums[i];
@@ -24,20 +26,22 @@ public:
                 suffix[i] = sum;
                 sum = 0;
             }
-        cout<<sum<<" ";
         }
-        cout<<endl;
-        ans = sum;
 
+// write logic here for update answer according to the given condition ;
         ans = max(ans,suffix[0]);
         for(int i=0;i<size-1;i++){
             if(nums[i] == 0){
                 ans = max(ans, perfix[i] + suffix[i]);
             }
         }
+
+        // one element should delete 
         if(ans == size){
             return ans-1;
         }
+
+        // return the answer here ;
         return ans;
     }
 };
