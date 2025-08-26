908c101f081ac1de2e8a96d2a06ae1d2492731ff
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-08-26 10:23:18 +0530

    Time: 51 ms (67.08%) | Memory: 129.3 MB (76.06%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -5,6 +5,7 @@ public:
         int n = grid[0].size();
         vector<vector<pair<int,int>>> matrix(m,vector<pair<int,int>> (n,{0,0}));
         int x = 0,y=0;
+        int ans = 0;
         for(int i=0;i<n;i++){
             if(grid[0][i] == 'X'){
                 x++;
@@ -13,6 +14,9 @@ public:
                 y++;
             }
             matrix[0][i] = {x,y};
+            if((matrix[0][i].first == matrix[0][i].second) && matrix[0][i].first){
+                ans++;
+            }
         }
 
         for(int i=1;i<m;i++){
@@ -27,14 +31,9 @@ public:
                 int newx = matrix[i-1][j].first;
                 int newy = matrix[i-1][j].second;
                 matrix[i][j] = {x+newx, y+newy};
-            }
-        }
-        int ans = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
                 if((matrix[i][j].first == matrix[i][j].second) && matrix[i][j].first){
-                    ans++;
-                }
+                ans++;
+            }
             }
         }
         return ans;
