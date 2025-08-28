69e208fb8f5aac570a5b42eb86f502ba07c35343
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-08-28 07:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (95.28%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> temp;
+        for(int r = m-1;r>=0;r--){
+            vector<int> temp;
+            int i = r;
+            int j = 0;
+            while(i<m && j<n){
+                temp.push_back(grid[i][j]);
+                i++;
+                j++;
+            }
+            sort(temp.begin(),temp.end());
+
+            i = r;
+            j = 0;
+            int k = 0;
+            while(i<m &&j<n){
+                grid[i][j] = temp[k];
+                i++;
+                j++;
+                k++;
+            }
+            temp.clear();
+        }
+
+        for(int r = 1;r<n;r++){
+            int i = 0;
+            int j = r;
+            while(i<m && j<n){
+                temp.push_back(grid[i][j]);
+                i++;
+                j++;
+            }
+            sort(temp.begin(),temp.end());
+
+            i = 0;
+            j = r;
+            int k = 0;
+            while(i<m &&j<n){
+                grid[i][j] = temp[k];
+                i++;
+                j++;
+                k++;
+            }
+            temp.clear();
+        }
+        return grid;
+    }
+};
