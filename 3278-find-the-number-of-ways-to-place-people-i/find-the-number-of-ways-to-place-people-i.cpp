259f26a4706f76bfab6f1239f2a16515833adebb
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-09-03 23:24:45 +0530

    Time: 763 ms (5.07%) | Memory: 32.6 MB (39.13%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int size = points.size();
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+    if (a[0] == b[0]) 
+        return a[1] > b[1];  // descending order of second term
+    return a[0] < b[0];      // ascending order of first term
+});
+
+        int ct = 0;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                if(!(x1<=x2 && y2<=y1)){
+                    cout<<"here "<<i<<j<<endl;
+                    continue;
+                }
+                bool b = true;
+                for(int k=0;k<size;k++){
+                    if(k==i || k==j){
+                        continue;
+                    }
+                    else{
+                        int x = points[k][0];
+                        int y = points[k][1];
+                        if((x1<=x && x<=x2) &&(y2<=y && y<=y1)){
+                            b = false;
+                            cout<<i<<j<<endl;                            
+                            break;
+                        }
+                    }
+                }
+                if(b == true){
+                    ct++;
+                }
+            }
+        }
+        return ct;
+    }
+};
