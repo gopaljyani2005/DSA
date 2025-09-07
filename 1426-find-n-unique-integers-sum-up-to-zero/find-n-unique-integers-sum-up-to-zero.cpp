6e871c5b815c025207714523b059758b57bb08c0
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-09-07 17:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (13.58%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -6,11 +6,11 @@ public:
             ans.push_back(0);
             n--;
         }
+
         for(int i=1;i<=n/2;i++){
             ans.push_back(i);
             ans.push_back(-i);
         }
-
         return ans;
     }
 };
