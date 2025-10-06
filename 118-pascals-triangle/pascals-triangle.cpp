0cc081eab24ce21ce0d03e998b761997a2c5d618
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-10-06 12:59:38 +0530

    Time: 2 ms (14.51%) | Memory: 9.6 MB (88.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> ans;
-        ans.push_back({1});
+        vector<vector<int>> ans(n);
+        ans[0] = {1};
         if(n==1){
             return ans;
         }
-        ans.push_back({1,1});
-        for(int i=2;i<n;i++){
-            vector<int> temp;
-            temp.push_back(1);
-            vector<int> last = ans[i-1];
-            for(int i=1;i<last.size();i++){
-                temp.push_back(last[i-1]+ last[i]);
+
+        for(int i=1;i<n;i++){
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++){
+                temp[j] = ans[i-1][j-1] + ans[i-1][j];
             }
-            temp.push_back(1);
-            ans.push_back(temp);
+            ans[i] = temp;
         }
         return ans;
     }
