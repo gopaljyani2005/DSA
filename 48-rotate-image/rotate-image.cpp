38e9cba21d712907709ba56410e90d429691f717
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-10-08 18:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.35%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> mat(n,vector<int>(m,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mat[j][m-1-i] = matrix[i][j];
+        int n = matrix.size(); // size of the matrix 
+        /// this solution is without extra space 
+
+        for(int i=0;i<(n)/2;i++){
+            for(int j=0;j<(n+1)/2;j++){
+                int temp = matrix[j][n-1-i];
+                matrix[j][n-1-i] = matrix[i][j];
+
+                int temp1 = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = temp;
+                temp = temp1;
+
+                temp1 = matrix[n-1-j][i];
+                matrix[n-1-j][i] = temp;
+                temp = temp1;
+
+                matrix[i][j] = temp;
             }
         }
-        matrix = mat;
+        return ;
     }
 };
