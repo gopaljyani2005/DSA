2e369b85e71358ba136daa680313103263d19acc
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-10-11 20:19:04 +0530

    Time: 57 ms (42.92%) | Memory: 51 MB (21.07%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int size = nums.size();
+        unordered_map<int,int> map;
+        map[0] = 1;
+        int sum = 0;
+        int ans =0;
+        for(int i=0;i<size;i++){
+            sum+=nums[i];
+            if(map[sum-k]){
+                ans+=map[sum-k];
+            }
+            map[sum]++;
+        }
+        return ans;
+
+    }
+};
