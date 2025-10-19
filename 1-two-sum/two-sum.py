44689ee6ef70c1830a381d884421c79a485acdca
GOPALA RAM JYANI <b22me075@iitj.ac.in>
2025-10-19 12:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (33.51%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mymap = {}
+        size = len(nums)
+        for i in range(0,size):
+            if target-nums[i] in mymap:
+                return [mymap[target-nums[i]],i]
+            else:
+                mymap[nums[i]] = i            
+            
+        
